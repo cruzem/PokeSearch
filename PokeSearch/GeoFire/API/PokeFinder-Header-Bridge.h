1c42a3dc233c401cb657c1bf27cd67d5f9530ad4
Emmanuel Cruz <cruz_emmanuelj@yahoo.com>
2017-04-11 09:45:16 -0400

    PokeSearch App Complete

---
+++ PokeSearch/GeoFire/API/PokeFinder-Header-Bridge.h
@@ -0,0 +1,13 @@
+//
+//  PokeFinder-Header-Bridge.h
+//  PokeSearch
+//
+//  Created by Emmanuel Cruz on 2/10/17.
+//  Copyright © 2017 DevPro. All rights reserved.
+//
+
+#ifndef PokeFinder_Header_Bridge_h
+#define PokeFinder_Header_Bridge_h
+
+
+#endif /* PokeFinder_Header_Bridge_h */
---
+++ PokeSearch/PokeSearch-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  PokeSearch-Bridging-Header.h
+//  PokeSearch
+//
+//  Created by Emmanuel Cruz on 1/19/17.
+//  Copyright © 2017 DevPro. All rights reserved.
+//
+
+#import "Geofire.h"
+
